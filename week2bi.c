26520d89ca54772987bc72dc43c4212b682d2b87
NaveenBen <30779207+NaveenBen@users.noreply.github.com>
2018-10-10 16:08:48 +0530

    Add files via upload

---
+++ week2bi.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<string.h>
+#include<fcntl.h>
+void server(int,int);
+void client(int,int);
+int main()
+{
+int p1[2],p2[2],pid;
+pipe(p1);
+pipe(p2);
+pid=fork();
+if(pid==0)
+{
+close(p1[1]);
+close(p2[0]);
+server(p1[0],p2[1]);
+return 0;
+}
+close(p1[0]);
+close(p2[1]);
+client(p1[1],p2[0]);
+wait();
+return 0;
+}
+void client(int wfd,int rfd)
+{
+int i,j,n;
+char fname[2000];
+char buff[2000];
+printf("ENTER THE FILE NAME :");
+scanf("%s",fname);
+printf("CLIENT SENDING THE REQUEST.....PLEASE WAIT\n");
+sleep(10);
+write(wfd,fname,2000);
+n=read(rfd,buff,2000);
+buff[n]='\0';
+printf("THE RESULTS OF CLIENTS ARE......\n");
+write(1,buff,n);
+}
+void server(int rfd,int wfd)
+{
+int i,j,n;
+char fname[2000];
+char buff[2000];
+n=read(rfd,fname,2000);
+fname[n]='\0';
+int fd=open(fname,O_RDONLY);
+sleep(10);
+if(fd<0)
+write(wfd,"can't open",9);
+else
+n=read(fd,buff,2000);
+write(wfd,buff,n);
+} 
+/*Input and Output:
+ENTER THE FILE NAME :namedpipes.txt
+CLIENT SENDING THE REQUEST.....PLEASE WAIT
+THE RESULTS OF CLIENTS ARE......
+Hi*/
